9cfaf5d8e266c6a53f3beeb7fc52747285afc5ee
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-11-28 18:16:20 -0300

    Redux devtools

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './store/reducers';
+import promiseMiddleware from 'redux-promise';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const createStoreWithMiddleware = composeEnhancers(
+  applyMiddleware(promiseMiddleware)
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
--- src/store/reducers/movies_reducer.js
+++ src/store/reducers/movies_reducer.js
@@ -1,9 +1,14 @@
-export default function appReducer(state={},action){
+const DEFAULT_STORE = {
+    userName:'Francis',
+    userId:1234
+}
+
+
+export default function appReducer(state=DEFAULT_STORE,action){
     switch(action.type){
         case 'MOVIES_LIST':
             return action.payload
         default:
             return state
     }
-
 }
